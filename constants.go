14bd8c169c5f1bf216c945e028aa955e7e9da9f2
Febrilian <febrilian.kr@gmail.com>
2023-01-22 12:43:29 +0700

    Added All Supported Languages according to https://developers.facebook.com/docs/whatsapp/api/messages/message-templates

--- constants.go
+++ constants.go
@@ -10,7 +10,243 @@ const (
 )
 
 var (
+	LanguageEnglish = TemplateLanguage{
+		Code: "en",
+	}
+	LanguageEnglishUS = TemplateLanguage{
+		Code: "en_US",
+	}
+	LanguageAfrikaans = TemplateLanguage{
+		Code: "af",
+	}
+	LanguageAlbanian = TemplateLanguage{
+		Code: "sq",
+	}
+	LanguageArabic = TemplateLanguage{
+		Code: "ar",
+	}
+	LanguageAzerbaijani = TemplateLanguage{
+		Code: "az",
+	}
+	LanguageBengali = TemplateLanguage{
+		Code: "bn",
+	}
+	LanguageBulgarian = TemplateLanguage{
+		Code: "bg",
+	}
+	LanguageCatalan = TemplateLanguage{
+		Code: "ca",
+	}
+
+	LanguageChineseCHN = TemplateLanguage{
+		Code: "zh_CN",
+	}
+	LanguageChineseHKG = TemplateLanguage{
+		Code: "zh_HK",
+	}
+
+	LanguageChineseTAI = TemplateLanguage{
+		Code: "zh_TW",
+	}
+
+	LanguageCroatian = TemplateLanguage{
+		Code: "hr",
+	}
+	LanguageCzech = TemplateLanguage{
+		Code: "cs",
+	}
+	LanguageDanish = TemplateLanguage{
+		Code: "da",
+	}
+	LanguageDutch = TemplateLanguage{
+		Code: "nl",
+	}
+
+	LanguageEstonian = TemplateLanguage{
+		Code: "et",
+	}
+	LanguageFilipino = TemplateLanguage{
+		Code: "fil",
+	}
+	LanguageFinnish = TemplateLanguage{
+		Code: "fi",
+	}
+	LanguageFrench = TemplateLanguage{
+		Code: "fr",
+	}
+	LanguageGeorgian = TemplateLanguage{
+		Code: "ka",
+	}
+	LanguageGerman = TemplateLanguage{
+		Code: "de",
+	}
+	LanguageGreek = TemplateLanguage{
+		Code: "el",
+	}
+	LanguageGujarati = TemplateLanguage{
+		Code: "gu",
+	}
+	LanguageHausa = TemplateLanguage{
+		Code: "ha",
+	}
+	LanguageHebrew = TemplateLanguage{
+		Code: "he",
+	}
+	LanguageHindi = TemplateLanguage{
+		Code: "hi",
+	}
+	LanguageHungarian = TemplateLanguage{
+		Code: "hu",
+	}
 	LanguageIndonesian = TemplateLanguage{
 		Code: "id",
+	}
+	LanguageIrish = TemplateLanguage{
+		Code: "ga",
+	}
+	LanguageItalian = TemplateLanguage{
+		Code: "it",
+	}
+	LanguageJapanese = TemplateLanguage{
+		Code: "ja",
+	}
+	LanguageKannada = TemplateLanguage{
+		Code: "kn",
+	}
+	LanguageKazakh = TemplateLanguage{
+		Code: "kk",
+	}
+	LanguageKinyarwanda = TemplateLanguage{
+		Code: "rw_RW",
+	}
+	LanguageKorean = TemplateLanguage{
+		Code: "ko",
+	}
+	LanguageKyrgyzKyrgyzstan = TemplateLanguage{
+		Code: "ky_KG",
+	}
+	LanguageLao = TemplateLanguage{
+		Code: "lo",
+	}
+	LanguageLatvian = TemplateLanguage{
+		Code: "lv",
+	}
+	LanguageLithuanian = TemplateLanguage{
+		Code: "lt",
+	}
+	LanguageMacedonian = TemplateLanguage{
+		Code: "mk",
+	}
+	LanguageMalay = TemplateLanguage{
+		Code: "ms",
+	}
+	LanguageMalayalam = TemplateLanguage{
+		Code: "ml",
+	}
+	LanguageMarathi = TemplateLanguage{
+		Code: "mr",
+	}
+	LanguageNorwegian = TemplateLanguage{
+		Code: "nb",
+	}
+
+	LanguagePersian = TemplateLanguage{
+		Code: "fa",
+	}
+
+	LanguagePolish = TemplateLanguage{
+		Code: "pl",
+	}
+
+	LanguagePortugueseBR = TemplateLanguage{
+		Code: "pt_BR",
+	}
+
+	LanguagePortuguesePOR = TemplateLanguage{
+		Code: "pt_PT",
+	}
+
+	LanguagePunjabi = TemplateLanguage{
+		Code: "pa",
+	}
+
+	LanguageRomanian = TemplateLanguage{
+		Code: "ro",
+	}
+
+	LanguageRussian = TemplateLanguage{
+		Code: "ru",
+	}
+
+	LanguageSerbian = TemplateLanguage{
+		Code: "sr",
+	}
+
+	LanguageSlovak = TemplateLanguage{
+		Code: "sk",
+	}
+
+	LanguageSlovenian = TemplateLanguage{
+		Code: "sl",
+	}
+
+	LanguageSpanish = TemplateLanguage{
+		Code: "es",
+	}
+
+	LanguageSpanishARG = TemplateLanguage{
+		Code: "es_AR",
+	}
+
+	LanguageSpanishSPA = TemplateLanguage{
+		Code: "es_ES",
+	}
+
+	LanguageSpanishMEX = TemplateLanguage{
+		Code: "es_MX",
+	}
+
+	LanguageSwahili = TemplateLanguage{
+		Code: "sw",
+	}
+
+	LanguageSwedish = TemplateLanguage{
+		Code: "sv",
+	}
+
+	LanguageTamil = TemplateLanguage{
+		Code: "ta",
+	}
+
+	LanguageTelugu = TemplateLanguage{
+		Code: "te",
+	}
+
+	LanguageThai = TemplateLanguage{
+		Code: "th",
+	}
+
+	LanguageTurkish = TemplateLanguage{
+		Code: "tr",
+	}
+
+	LanguageUkrainian = TemplateLanguage{
+		Code: "uk",
+	}
+
+	LanguageUrdu = TemplateLanguage{
+		Code: "ur",
+	}
+
+	LanguageUzbek = TemplateLanguage{
+		Code: "uz",
+	}
+
+	LanguageVietnamese = TemplateLanguage{
+		Code: "vi",
+	}
+
+	LanguageZulu = TemplateLanguage{
+		Code: "zu",
 	}
 )
